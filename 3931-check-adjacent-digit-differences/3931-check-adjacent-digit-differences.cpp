35a47be7c4a703406c3c5d9488cf0c35081f10c1
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-05-20 14:45:39 +0530

    Time: 1 ms (18.67%), Space: 8.4 MB (97.59%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n = s.size();
+        for(int i=0;i<n-1;i++)
+        {
+            int ans = abs((s[i]-'0')-(s[i+1]-'0'));
+            if(ans>2)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
